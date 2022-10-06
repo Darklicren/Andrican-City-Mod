a4aa8cb1935a80f429017cf87da14e84856a5159
Darklicren <darklicren@gmail.com>
2022-10-06 11:50:35 +0800

    Adding this

---
+++ src/main/java/net/darklicren/andricancity/AndricanCityClient.java
@@ -0,0 +1,10 @@
+package net.darklicren.andricancity;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AndricanCityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
